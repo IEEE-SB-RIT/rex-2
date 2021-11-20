481a06f276f556425a3c9d48b3a5459fa86e9ca7
Navaneeth Krishna E S <naveekurosaki@gmail.com>
2021-11-20 10:19:37 +0530

    Initial build subtree commit

--- src/components/Footer/FooterElements.js
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components"
-import {Section} from "../Center/CenterElements"
-
-export const FooterSection = styled(Section)`
-    background: radial-gradient(circle at top,#0F2D4A, #111015);
-    min-height: 400px;
-    color: white;
-`
