e49fc596b433860bc396bb85f3db7b66339efd84
Jobin Biju <jobinbiju9090@gmail.com>
2021-11-22 18:50:54 +0530

    Changed contact and coutdown

--- src/components/ContactCard/index.js
+++ src/components/ContactCard/index.js
@@ -1,33 +1,50 @@
-import React from "react";
-import { QueryCard, Column, VolunteerContactInfo, Person } from "./ContactCardElements";
+import React from 'react';
+import {
+  QueryCard,
+  Column,
+  VolunteerContactInfo,
+  Person,
+} from './ContactCardElements';
 
-export default function ContactCard()
-{
-    return (
-
-        <>
-            <QueryCard>
-                <Column>
-                <h1>Have any Queries?</h1>
-                <p>Feel free to get in touch with us.</p>
-                </Column>
-                <Column>
-                    <p>Get in touch with our very friendly volunteers</p>
-                    <VolunteerContactInfo>
-                        <Person>
-                            <p><b>Jake Peralta</b></p>
-                            <p>jake@nine-nine.org<br/>+91 1234567890</p>
-                        </Person>
-                        <Person>
-                            <p><b>Jonas Kahnwald</b></p>
-                            <p>jonas@dark.org<br/>+91 1234567890</p>
-                        </Person>
-                    </VolunteerContactInfo>
-                    <hr />
-                    <p>Or send us an email regarding your queries @ <strong>ieeesb.ritk@gmail.com</strong></p>
-                </Column>
-            </QueryCard>
-        </>
-
-    );
+export default function ContactCard() {
+  return (
+    <>
+      <QueryCard>
+        <Column>
+          <h1>Have any Queries?</h1>
+          <p>Feel free to get in touch with us.</p>
+        </Column>
+        <Column>
+          <p>Get in touch with our very friendly volunteers</p>
+          <VolunteerContactInfo>
+            <Person>
+              <p>
+                <b>Abdul Ahad</b>
+              </p>
+              <p>
+                abdulahad01@ieee.org
+                <br />
+                +91 6238792719
+              </p>
+            </Person>
+            <Person>
+              <p>
+                <b>Sidharth S</b>
+              </p>
+              <p>
+                sidharth248@ieee.org
+                <br />
+                +91 9383429474
+              </p>
+            </Person>
+          </VolunteerContactInfo>
+          <hr />
+          <p>
+            Or send us an email regarding your queries @{' '}
+            <strong>ieeesb.ritk@gmail.com</strong>
+          </p>
+        </Column>
+      </QueryCard>
+    </>
+  );
 }
--- src/components/EventSection/index.js
+++ src/components/EventSection/index.js
@@ -1,39 +1,66 @@
-import React from "react";
-import { GradientCard, Event } from "./EventSectionElements";
+import React from 'react';
+import { GradientCard, Event } from './EventSectionElements';
+import eventpic from '../../images/preRex2.jpeg';
 
-export default function EventSection()
-{
-    return (
-        <>  
-                    
-                    <h1><span>E</span>VENTS</h1>
-                    <p>Stay tune till we reveal our amazing and exciting events!</p>
-                    
-                    <svg width="244" height="394" viewBox="0 0 244 394" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <circle class="dashed-circle" cx="197" cy="197" r="196.5" stroke="#1D4A9B" stroke-linecap="round" stroke-dasharray="3 10"/>
-                    </svg>
+export default function EventSection() {
+  return (
+    <>
+      <h1>
+        <span>E</span>VENTS
+      </h1>
+      <p>Stay tune till we reveal our amazing and exciting events!</p>
 
+      <svg
+        width="244"
+        height="394"
+        viewBox="0 0 244 394"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <circle
+          class="dashed-circle"
+          cx="197"
+          cy="197"
+          r="196.5"
+          stroke="#1D4A9B"
+          stroke-linecap="round"
+          stroke-dasharray="3 10"
+        />
+      </svg>
 
-                    <GradientCard>
-                        <Event>
-                            <h3>Pre-events</h3>                            
-                        </Event>
-                    
-                        <Event>
-                            <h3>Panel Discussions</h3>                            
-                        </Event>
-                    
-                        <Event>
-                            <h3>Workshops</h3>                            
-                        </Event>
+      <GradientCard>
+        <Event>
+          <h3>Pre-events</h3>
+          <br />
+          <img src={eventpic} alt="Pre Event" height={200} />
+        </Event>
 
-                    </GradientCard>
-                    
-                    <svg width="275" height="394" viewBox="0 0 275 394" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <circle class="dashed-circle" cx="78" cy="197" r="196.5" stroke="#BD00FF" stroke-linecap="round" stroke-dasharray="3 10"/>
-                    </svg>
+        <Event>
+          <h3>Panel Discussions</h3>
+        </Event>
 
+        <Event>
+          <h3>Workshops</h3>
+        </Event>
+      </GradientCard>
 
-        </>
-    );
+      <svg
+        width="275"
+        height="394"
+        viewBox="0 0 275 394"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <circle
+          class="dashed-circle"
+          cx="78"
+          cy="197"
+          r="196.5"
+          stroke="#BD00FF"
+          stroke-linecap="round"
+          stroke-dasharray="3 10"
+        />
+      </svg>
+    </>
+  );
 }
