bc264c602c1c9f64122f2fcbeab38779c2245827
Aswin K R <aswinkr77@gmail.com>
2021-12-10 23:42:39 +0530

    added workshop 3

--- src/pages/workshops.js
+++ src/pages/workshops.js
@@ -5,6 +5,7 @@ import JoinButton from "../components/Button";
 import { ButtonContainer } from "../components/Button/ButtonElements";
 import workshop1 from '../images/rex-workshop1.jpeg';
 import workshop2 from '../images/rex-workshop2.jpeg';
+import workshop3 from '../images/workshop-drone.jpeg';
 
 export default function Workshops()
 {
@@ -15,6 +16,13 @@ export default function Workshops()
     return (<>
         <CenteredLayout title={pageDetails.title} subtitle={pageDetails.subtitle}>
             <WorkshopCard>
+                <h3>DRONE - ZONE : Workshop on Drone Software</h3>
+                <img src={workshop3} width="200px"/>
+                <ButtonContainer>
+            <a href="https://bit.ly/drone-zone" target="_blank" rel="noreferrer"><JoinButton>Register Now</JoinButton></a>
+          </ButtonContainer>
+            </WorkshopCard>
+            <WorkshopCard>
                 <h3>Introduction to Robotics and Joy of Flying</h3>
                 <img src={workshop1} width="200px"/>
                 <ButtonContainer>
@@ -22,9 +30,11 @@ export default function Workshops()
           </ButtonContainer>
             </WorkshopCard>
             <WorkshopCard>
-                <h3>Introduction to Robotics and Joy of Flying</h3>
+                <h3>Computer Vision for Robotics</h3>
                 <img src={workshop2} height="200px"/>
+                <ButtonContainer>
                 <a href="https://www.yepdesk.com/rex-2-0"><JoinButton>Register Now</JoinButton></a>
+                </ButtonContainer>
             </WorkshopCard>
         </CenteredLayout>
     </>)
